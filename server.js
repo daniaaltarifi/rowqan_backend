05e82f969ba007987d634faf6d6ed75596eaee51
loransalkhateeb <lwrnsalkhtyb9@gmail.com>
2025-01-08 16:53:38 +0300

    Update

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
+
 const express = require('express');
 const sequelize = require('./Config/dbConnect');
 const helmet = require('helmet');
@@ -7,6 +8,12 @@ const socketIo = require('socket.io');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
+
+
+
+
+
+
 
 const app = express();
 const compression = require("compression");
@@ -28,6 +35,10 @@ app.use(
     crossOriginEmbedderPolicy: false,
   })
 );
+
+// const server = http.createServer(app);
+// const io = socketIo(server);
+
 app.use((req, res, next) => {
   req.socketIoInstance = io;
   next();
@@ -45,8 +56,19 @@ app.use(
 
 app.use(express.json());
 
+// const corsOptions = {
+//   origin: "http://localhost:5173",  // Replace with your frontend URL
+//   methods: ["GET", "POST"],
+//   allowedHeaders: ["Content-Type"],
+// };
+
+// // Apply CORS middleware to the express app
+// app.use(cors(corsOptions));
 
 // Create HTTP server and attach Socket.IO
+
+
+
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
@@ -54,12 +76,11 @@ const io = socketIo(server, {
       "http://localhost:5173",
       "https://rowqan.com",
       "https://rowqanbackend.rowqan.com",
-    ], // Allow frontend to connect
+    ], 
     methods: ["GET", "POST"],
   },
 });
 
-// Example Socket.IO connection event
 
 // io.on("connection", (socket) => {
 //   console.log("a user connected");
@@ -122,11 +143,12 @@ const AboutRoutes = require('./Routes/AboutUsRoutes')
 const BlogRoutes = require('./Routes/BlogRoutes')
 
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
 
-  // Handle message events
-  socket.on("send_message", (data) => {
+
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
   socket.on("send_message", (message) => {
     console.log("Message received: ", message);
     io.emit("receive_message", message);
@@ -134,63 +156,23 @@ io.on("connection", (socket) => {
 
   socket.on('receive_message', (data) => {
     console.log("Message received:", data);
-    // Broadcast to other users
-    io.emit("receive_message", data);
   });
 
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    console.log("A user disconnected");
   });
 });
 
-const UsersRoutes = require("./Routes/UsersRoutes");
-const LogoRoutes = require("./Routes/LogoRoutes");
-const HeaderRoutes = require("./Routes/HeaderRoutes");
-const HeroesRoutes = require("./Routes/HeroRoutes");
-const ServicesRoutes = require("./Routes/ServicesRoutes");
-const FooterRoutes = require("./Routes/FooterRoutes");
-const FooterIconRoutes = require("./Routes/FooterIconsRoutes");
-const HeroChaletsRoutes = require("./Routes/ChaletsHeroRoutes");
-const ChaletsRoutes = require("./Routes/ChaletsRoutes");
-const statusChaletRoutes = require("./Routes/StatusChaletsRoutes");
-const ChaletImagesRoutes = require("./Routes/ChaletsImagesRoutes");
-const BreifDetailsChaletsRoutes = require("./Routes/BreifDetailsChaletsRoutes");
-const ReservatioDatesRoutes = require("./Routes/ReservationsDateRoutes");
-const ContactUsRoutes = require("./Routes/ContactUsRoutes");
-const RightTimeRoutes = require("./Routes/RightTimeRoutes");
-const StatusRoutes = require("./Routes/StatusRoutes");
-const ChaletsDetailsRoutes = require("./Routes/ChaletsDetailsRoutes");
-const HeroEventsRoutes = require("./Routes/EventsHeroRoutes");
-const EventsTypesRoutes = require("./Routes/TypesEventsRoutes");
-const SubEventsRoutes = require("./Routes/SubEventsRoutes");
-const AvailableEventsRoutes = require("./Routes/AvailableEventsRoutes");
-const AvailableImages = require("./Routes/AvailableImagesRoutes");
-const PlansRoutes = require("./Routes/PlansRoutes");
-const ReservatioEventsRoutes = require("./Routes/ReservationsEventsRoutes");
-const CategoryLandsRoutes = require("./Routes/CategoriesLandsRoutes");
-const PrpertyLandsRoutes = require("./Routes/PropertiesLandsRoutes");
-const ImagesLandsRoutes = require("./Routes/CategoriesImagesRoutes");
-const BreifLandsRoutes = require("./Routes/BriefLandsRoutes");
-const ReservationeLandsRoutes = require("./Routes/ReservationsLandsRoutes");
-const ReservationsRoutes = require("./Routes/ReservationsRoutes");
-const UsersTypesRoutes = require("./Routes/UsersTypesRoutes");
-const ReservationsChaletsRoutes = require("./Routes/ReservationsChaletsRoutes");
-const WalletRoutes = require("./Routes/WalletRoutes");
-const PropsChaletsRoutes = require("./Routes/ChaletsPropsRoutes");
-const FeedBackRoutes = require("./Routes/FeedBacksRoutes");
-const MessagesRoutes = require("./Routes/MessagesRoutes");
-const HeroLands = require("./Routes/HeroLandsRoutes");
-const PaymentsRoutes = require("./Routes/PaymentsRoutes");
-const AboutRoutes = require('./Routes/AboutusRoutes')
-const BlogRoutes = require('./Routes/BlogRoutes')
+
 
 
 const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:5173",
-  "https://rowqan.com",
-  "https://rowqanbackend.rowqan.com",
+  'http://localhost:5173',
+  'http://localhost:3001',
+  'https://rowqan.com',
+  'https://rowqanbackend.rowqan.com',
 ];
+
 
 const corsOptions = {
   origin: (origin, callback) => {
@@ -250,6 +232,54 @@ app.use('/payments', PaymentsRoutes);
 app.use('/aboutUs',AboutRoutes)
 app.use('/Blogs',BlogRoutes)
 
+
+
+
+
+
+
+
+
+app.use("/users", UsersRoutes);
+app.use("/logos", LogoRoutes);
+app.use("/header", HeaderRoutes);
+app.use("/heroes", HeroesRoutes);
+app.use("/services", ServicesRoutes);
+app.use("/footer", FooterRoutes);
+app.use("/footericons", FooterIconRoutes);
+app.use("/heroChalets", HeroChaletsRoutes);
+app.use("/chalets", ChaletsRoutes);
+app.use("/statuschalets", statusChaletRoutes);
+app.use("/chaletsimages", ChaletImagesRoutes);
+app.use("/BreifDetailsChalets", BreifDetailsChaletsRoutes);
+app.use("/ReservationDates", ReservatioDatesRoutes);
+app.use("/ContactUs", ContactUsRoutes);
+app.use("/RightTimes", RightTimeRoutes);
+app.use("/status", StatusRoutes);
+app.use("/chaletsdetails", ChaletsDetailsRoutes);
+app.use("/heroevents", HeroEventsRoutes);
+app.use("/events", EventsTypesRoutes);
+app.use("/subevents", SubEventsRoutes);
+app.use("/availablevents", AvailableEventsRoutes);
+app.use("/availableimages", AvailableImages);
+app.use("/plans", PlansRoutes);
+app.use("/reservationsEvents", ReservatioEventsRoutes);
+app.use("/categorieslands", CategoryLandsRoutes);
+app.use("/propertyLands", PrpertyLandsRoutes);
+app.use("/imageslands", ImagesLandsRoutes);
+app.use("/BreifLands", BreifLandsRoutes);
+app.use("/reservationLands", ReservationeLandsRoutes);
+app.use("/reservations", ReservationsRoutes);
+app.use("/userstypes", UsersTypesRoutes);
+app.use("/ReservationsChalets", ReservationsChaletsRoutes);
+app.use("/Wallet", WalletRoutes);
+app.use("/propschalets", PropsChaletsRoutes);
+app.use("/FeedBacks", FeedBackRoutes);
+app.use("/messages", MessagesRoutes);
+app.use("/heroLands", HeroLands);
+app.use("/payments", PaymentsRoutes);
+app.use('/aboutUs',AboutRoutes)
+app.use('/Blogs',BlogRoutes)
 const IP_LOOKUP_API =
   "https://ipqualityscore.com/api/json/ip/T0hMeOnMzeAnPVsmgH6AKMhguvmr1Yv9";
 
