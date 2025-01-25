af89d5bedb275a54452fa9ce3cca2c8634ed588a
loransalkhateeb <lwrnsalkhtyb9@gmail.com>
2025-01-25 14:14:24 +0300

    New Update

--- Controllers/UsersController.js
+++ Controllers/UsersController.js
@@ -208,8 +208,8 @@ exports.updateUser = async (req, res) => {
 
 
 exports.deleteUser = async (req, res) => {
-  const { id } = req.params;
-  const { lang } = req.query; 
+  const { id,lang } = req.params;
+
   
   try {
     
