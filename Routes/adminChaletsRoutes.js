86f125a658b4bc9663ddcdde3ff5b2e40cc3b863
lorans alkhateeb <lwrnsalkhtyb9@gmail.com>
2025-03-01 10:31:17 +0300

    create of getUserIdByChaletId function in lorans2 branch

--- Routes/adminChaletsRoutes.js
+++ Routes/adminChaletsRoutes.js
@@ -7,7 +7,7 @@ const rateLimiter = require('../MiddleWares/rateLimiter');
 
 router.get('/getChaletsByAdminId/:userId',rateLimiter,AdminChaletsController.getChaletByUserId)
 
-
+router.get('/getUserIdByChaletId/:chaletId',rateLimiter,AdminChaletsController.getUserIdByChaletId)
 
 
 module.exports = router;
